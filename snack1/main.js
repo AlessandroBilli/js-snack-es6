8708e1aec50926dcd693ccf81ba4ee953c323e8e
AlessandroBilli <skillmanbungo@gmail.com>
2023-11-22 18:46:26 +0100

    feat: Bici js

--- snack1/main.js
+++ snack1/main.js
@@ -1,1 +1,25 @@
 'use strict'
+
+const bici = [
+    {
+        Nome: 'Fulmine Giallo',
+        Peso: '9 kg',
+        
+    },
+    {
+        Nome: 'Scheggia Rossa',
+        Peso:'11 kg',
+        
+    },
+    {
+        Nome: 'Onda Blu',
+        Peso:'8 kg',
+       
+    },
+    {
+        Nome: 'Freccia Argento',
+        Peso:'10 kg',
+    
+    },
+ 
+];
