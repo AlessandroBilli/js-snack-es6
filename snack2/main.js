5b1958e5f0a1bdbe3ea9151afce9d47ae42f85c5
AlessandroBilli <skillmanbungo@gmail.com>
2023-11-23 00:43:31 +0100

    feat: snack2

--- snack2/main.js
+++ snack2/main.js
@@ -1,1 +1,22 @@
 'use strict'
+
+function generaNumeroCasuale(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const squadre = [
+    { nome: 'Juventus', puntiFatti: 0, falliSubiti: 0 },
+    { nome: 'Inter', puntiFatti: 0, falliSubiti: 0 },
+    { nome: 'Milan', puntiFatti: 0, falliSubiti: 0 },
+    { nome: 'Roma', puntiFatti: 0, falliSubiti: 0 }
+];
+
+squadre.forEach(squadra => {
+    squadra.puntiFatti = generaNumeroCasuale(0, 60);
+    squadra.falliSubiti = generaNumeroCasuale(0, 30);
+});
+
+const squadreNomiFalli = squadre.map(({ nome, falliSubiti }) => ({ nome, falliSubiti }));
+
+console.log("Squadre con nomi e falli subiti:");
+console.log(squadreNomiFalli);
